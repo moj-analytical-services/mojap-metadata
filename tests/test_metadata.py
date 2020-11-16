2a37a0248c92c37ada334c039f69d106e95622ea
calumabarnett <calum.barnett@digital.justice.gov.uk>
2020-11-16 21:22:34 +0000

    Refactor

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,6 +1,6 @@
-from typing import Any, Type
-from attr import attr, attributes, set_run_validators
-import py
+from typing import Any
+
+from jsonschema.exceptions import ValidationError
 
 import pytest
 from mojap_metadata.metadata.metadata import Metadata
@@ -25,51 +25,48 @@ class TestMetadata:
         setattr(metadata, attribute, valid_value)
         assert getattr(metadata, attribute) == valid_value
 
-        with pytest.raises(TypeError):
+        with pytest.raises(ValidationError):
             setattr(metadata, attribute, invalid_value)
 
-    def test_columns(self):
-        metadata = Metadata()
-        assert metadata.columns == list()
-        metadata.columns = [{"name": "col1"}, {"name": "col2"}]
-        assert metadata.columns == [{"name": "col1"}, {"name": "col2"}]
-
-        with pytest.raises(TypeError):
-            setattr(metadata, "columns", 0)
-
-        with pytest.raises(TypeError):
-            setattr(metadata, "columns", [0])
-
-        with pytest.raises(TypeError):
-            setattr(metadata, "columns", [{"name": 0}])
-
-    def _test_with_columns(self, attribute, default_value, valid_value, invalid_value):
-        metadata = Metadata()
-        assert getattr(metadata, attribute) == default_value
-
-        with pytest.raises(ValueError):
-            setattr(metadata, attribute, valid_value)
-
-        with pytest.raises(TypeError):
-            setattr(metadata, attribute, invalid_value)
-
-        with pytest.raises(ValueError):
-            setattr(metadata, attribute, valid_value * 2)
-
-        metadata.columns = [{"name": "test"}]
-        setattr(metadata, attribute, valid_value)
-        assert getattr(metadata, attribute) == ["test"]
-
-    @pytest.mark.parametrize(
-        argnames="attribute,default_value,valid_value,invalid_value",
-        argvalues=[("primary_key", [], ["test"], 0), ("partitions", [], ["test"], 0)],
-    )
-    def test_primary_key_and_partitions(
-        self, attribute: str, default_value: Any, valid_value: Any, invalid_value: Any
-    ):
-        self._test_with_columns(
-            attribute=attribute,
-            default_value=default_value,
-            valid_value=valid_value,
-            invalid_value=invalid_value,
+    def test_from_dict(self):
+        Metadata().from_dict(
+            {
+                "name": "test",
+                "description": "test",
+                "format": "test",
+                "sensitive": False,
+                "columns": [{"name": "test", "type": "int8"}],
+                "primary_key": ["test"],
+                "partitions": ["test"],
+            }
         )
+
+        with pytest.warns(
+            UserWarning, match="Some properties will be ignored: prop_1, prop_2"
+        ):
+            Metadata().from_dict(
+                {
+                    "prop_1": None,
+                    "prop_2": None,
+                }
+            )
+
+    def test_to_dict(self):
+        metadata = Metadata(
+            name="test",
+            description="test",
+            format="test",
+            sensitive=False,
+            columns=[{"name": "test", "type": "int8"}],
+            primary_key=["test"],
+            partitions=["test"],
+        )
+        assert metadata.to_dict() == {
+            "name": "test",
+            "description": "test",
+            "format": "test",
+            "sensitive": False,
+            "columns": [{"name": "test", "type": "int8"}],
+            "primary_key": ["test"],
+            "partitions": ["test"],
+        }
