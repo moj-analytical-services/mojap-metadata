e2d5987d9f82d20cd5ad067c49d89d8619979d52
nicholsondarius <darius.nicholson@digital.justice.gov.uk>
2020-11-17 17:50:26 +0000

    Update unit tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -12,13 +12,17 @@ class TestMetadata:
         argvalues=[
             ("name", "", "test", 0),
             ("description", "", "test", 0),
-            ("format", "", "test", 0),
+            ("file_format", "", "test", 0),
             ("sensitive", False, True, 0),
         ],
     )
-    def test_attributes(
+    def test_basic_attributes(
         self, attribute: str, default_value: Any, valid_value: Any, invalid_value: Any
     ):
+        """
+        Attributes with default, valid and invalid types are handled as
+        expected.
+        """
         metadata = Metadata()
         assert getattr(metadata, attribute) == default_value
 
@@ -28,14 +32,100 @@ class TestMetadata:
         with pytest.raises(ValidationError):
             setattr(metadata, attribute, invalid_value)
 
+    def test_columns_default(self):
+        metadata = Metadata()
+        assert metadata.columns == []
+
+    @pytest.mark.parametrize(
+        argnames="input",
+        argvalues=[
+            "",
+            [""],
+            [{"name": "test"}],
+            [{"type_category": "integer"}],
+            [{"type": "null"}],
+            [{"name": "test", "type_category": "test"}],
+            [{"name": 0, "type_category": "integer"}],
+            [{"name": "test", "type_category": 0}],
+            [{"name": "test", "type": 0}],
+            [{"name": "test", "type": "int7"}],
+            [{"name": "test", "type": "kint8"}],
+            [{"name": "test", "type": "float8"}],
+            [{"name": "test", "type": "decimal"}],
+            [{"name": "test", "type": "decimal(0,38)"}],
+            [{"name": "test", "type_category": "null", "type": "test"}],
+            [{"name": "test", "type_category": "integer", "type": "test"}],
+        ],
+    )
+    def test_columns_validation_error(self, input: Any):
+        metadata = Metadata()
+        with pytest.raises(ValidationError):
+            metadata.columns = input
+
+    @pytest.mark.parametrize(
+        argnames="input",
+        argvalues=[
+            [{"name": "test", "type_category": "null"}],
+            [{"name": "test", "type_category": "integer"}],
+            [{"name": "test", "type_category": "float"}],
+            [{"name": "test", "type_category": "string"}],
+            [{"name": "test", "type_category": "datetime"}],
+            [{"name": "test", "type_category": "binary"}],
+            [{"name": "test", "type_category": "boolean"}],
+            [{"name": "test", "type": "int8"}],
+            [{"name": "test", "type": "int16"}],
+            [{"name": "test", "type": "int32"}],
+            [{"name": "test", "type": "int64"}],
+            [{"name": "test", "type": "uint8"}],
+            [{"name": "test", "type": "uint16"}],
+            [{"name": "test", "type": "uint32"}],
+            [{"name": "test", "type": "uint64"}],
+            [{"name": "test", "type": "float16"}],
+            [{"name": "test", "type": "float32"}],
+            [{"name": "test", "type": "float64"}],
+            [{"name": "test", "type": "decimal128(0,38)"}],
+            [{"name": "test", "type": "time32"}],
+            [{"name": "test", "type": "time64"}],
+            [{"name": "test", "type": "time64(s)"}],
+            [{"name": "test", "type": "time64(ms)"}],
+            [{"name": "test", "type": "timestamp"}],
+            [{"name": "test", "type": "timestamp(s)"}],
+            [{"name": "test", "type": "timestamp(ms)"}],
+            [{"name": "test", "type": "timestamp(us)"}],
+            [{"name": "test", "type": "timestamp(ns)"}],
+            [{"name": "test", "type": "date32"}],
+            [{"name": "test", "type": "date64"}],
+            [{"name": "test", "type": "string"}],
+            [{"name": "test", "type": "large_string"}],
+            [{"name": "test", "type": "utf8"}],
+            [{"name": "test", "type": "large_utf8"}],
+            [{"name": "test", "type": "binary"}],
+            [{"name": "test", "type": "binary(128)"}],
+            [{"name": "test", "type": "large_binary"}],
+            [{"name": "test", "type_category": "null", "type": "null"}],
+            [{"name": "test", "type_category": "integer", "type": "int8"}],
+            [{"name": "test", "type_category": "float", "type": "float32"}],
+            [{"name": "test", "type_category": "string", "type": "string"}],
+            [{"name": "test", "type_category": "datetime", "type": "timestamp"}],
+            [{"name": "test", "type_category": "binary", "type": "binary(128)"}],
+            [{"name": "test", "type_category": "binary", "type": "binary"}],
+            [{"name": "test", "type_category": "boolean", "type": "bool_"}],
+        ],
+    )
+    def test_columns_pass(self, input: Any):
+        Metadata(columns=input)
+
+    def test_primary_key_and_partitions_attributes(self):
+        pass
+
     def test_from_dict(self):
         Metadata().from_dict(
             {
                 "name": "test",
                 "description": "test",
-                "format": "test",
+                "file_format": "test",
                 "sensitive": False,
-                "columns": [{"name": "test", "type": "int8"}],
+                "columns": [{"name": "test", "type": "null"}],
                 "primary_key": ["test"],
                 "partitions": ["test"],
             }
@@ -55,18 +145,18 @@ class TestMetadata:
         metadata = Metadata(
             name="test",
             description="test",
-            format="test",
+            file_format="test",
             sensitive=False,
-            columns=[{"name": "test", "type": "int8"}],
+            columns=[{"name": "test", "type": "null"}],
             primary_key=["test"],
             partitions=["test"],
         )
         assert metadata.to_dict() == {
             "name": "test",
             "description": "test",
-            "format": "test",
+            "file_format": "test",
             "sensitive": False,
-            "columns": [{"name": "test", "type": "int8"}],
+            "columns": [{"name": "test", "type": "null"}],
             "primary_key": ["test"],
             "partitions": ["test"],
         }
