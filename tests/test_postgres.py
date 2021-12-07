3c2959444f255fc0cb9740e61f0942f791442232
priyabasker23 <shanmugapriya.basker@digital.justice.gov.uk>
2021-12-07 04:40:15 +0000

    Added extra unit test

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 
 from mojap_metadata.extractors import postgres_metadata as m
@@ -8,90 +9,118 @@ from pathlib import Path
 TEST_ROOT = Path(__file__).resolve().parent
 
 
-class TestPostgressExtractorClass:
-    # @pytest.fixture(scope="module", autouse=True)
-    def load_data(self, postgres_connection):
+# @pytest.fixture(scope="module", autouse=True)
+def load_data(postgres_connection):
 
-        path = TEST_ROOT / "data/postgres_extractor"
-        files = sorted(Path.glob(path, "postgres*.csv"))
-        engine = postgres_connection[0]
+    path = TEST_ROOT / "data/postgres_extractor"
+    files = sorted(Path.glob(path, "postgres*.csv"))
+    engine = postgres_connection[0]
 
-        for file in files:
-            # Read file
-            tabledf = pd.read_csv(str(file), index_col=None)
+    for file in files:
+        # Read file
+        tabledf = pd.read_csv(str(file), index_col=None)
 
-            # Create table
-            filename = str(file).rsplit("/")[-1].replace(".csv", "")
+        # Create table
+        filename = str(file).rsplit("/")[-1].replace(".csv", "")
 
-            # Define datatype
-            dtypedict = {
-                "my_int": Integer,
-                "my_float": Float,
-                "my_decimal": Float,
-                "my_bool": Boolean,
-                "my_website": String,
-                "my_email": String,
-                "my_datetime": DateTime,
-                "my_date": Date,
-                "primary_key": Integer,
-            }
-
-            tabledf.to_sql(
-                filename,
-                engine,
-                if_exists="replace",
-                index=False,
-                dtype=dtypedict,
-            )
-
-            # Sample comment for column for testing
-            engine.connect().execute(
-                """COMMENT ON COLUMN public.postgres_table1.my_int"""
-                """ IS 'This is the int column';COMMIT;"""
-            )
-
-    def test_connection(self, postgres_connection):
-        pgsql = postgres_connection[1]
-        engine = postgres_connection[0]
-        assert engine is not None
-        assert ("system is ready to accept connections") in pgsql.read_bootlog()
-
-    def test_dsn_and_url(self, postgres_connection):
-        pgsql = postgres_connection[1]
-        expected = {
-            "port": 5433,
-            "host": "127.0.0.1",
-            "user": "postgres",
-            "database": "test",
+        # Define datatype
+        dtypedict = {
+            "my_int": Integer,
+            "my_float": Float,
+            "my_decimal": Float,
+            "my_bool": Boolean,
+            "my_website": String,
+            "my_email": String,
+            "my_datetime": DateTime,
+            "my_date": Date,
+            "primary_key": Integer,
         }
 
-        assert pgsql.dsn() == expected
-        assert pgsql.url() == "postgresql://postgres@127.0.0.1:5433/test"
+        tabledf.to_sql(
+            filename,
+            engine,
+            if_exists="replace",
+            index=False,
+            dtype=dtypedict,
+        )
 
-    def test_meta_data_object_list(self, postgres_connection):
-        engine = postgres_connection[0].connect()
-        self.load_data(postgres_connection)
-        output = m.get_object_meta_for_all_tables(engine)
+        # Sample comment for column for testing
+        engine.connect().execute(
+            """COMMENT ON COLUMN public.postgres_table1.my_int"""
+            """ IS 'This is the int column';COMMIT;"""
+        )
 
-        for i in output.items():
-            assert len(i[1]) == 2
-            assert i[0] == "schema: public"
 
-    def test_meta_data_object(self, postgres_connection):
-        engine = postgres_connection[0].connect()
-        self.load_data(postgres_connection)
-        meta = m.get_object_meta(engine, "postgres_table1", "public")
-        print(meta.columns)
-        assert len(meta.columns) == 9
-        assert meta.columns[0]["description"] == "This is the int column"
-        assert meta.column_names == [
-            "my_int",
-            "my_float",
-            "my_decimal",
-            "my_bool",
-            "my_website",
-            "my_email",
-            "my_datetime",
-            "my_date",
-            "primary_key",
-        ]
+def test_connection(postgres_connection):
+    pgsql = postgres_connection[1]
+    engine = postgres_connection[0]
+    assert engine is not None
+    assert ("system is ready to accept connections") in pgsql.read_bootlog()
+
+
+def test_dsn_and_url(postgres_connection):
+    pgsql = postgres_connection[1]
+    expected = {
+        "port": 5433,
+        "host": "127.0.0.1",
+        "user": "postgres",
+        "database": "test",
+    }
+
+    assert pgsql.dsn() == expected
+    assert pgsql.url() == "postgresql://postgres@127.0.0.1:5433/test"
+
+
+def test_meta_data_object_list(postgres_connection):
+    engine = postgres_connection[0].connect()
+    load_data(postgres_connection)
+    output = m.get_object_meta_for_all_tables(engine)
+
+    for i in output.items():
+        assert len(i[1]) == 2
+        assert i[0] == "schema: public"
+
+
+def test_meta_data_object(postgres_connection):
+    engine = postgres_connection[0].connect()
+    load_data(postgres_connection)
+    meta = m.get_object_meta(engine, "postgres_table1", "public")
+    print(meta.columns)
+    assert len(meta.columns) == 9
+    assert meta.columns[0]["description"] == "This is the int column"
+    assert meta.column_names == [
+        "my_int",
+        "my_float",
+        "my_decimal",
+        "my_bool",
+        "my_website",
+        "my_email",
+        "my_datetime",
+        "my_date",
+        "primary_key",
+    ]
+
+
+@pytest.mark.parametrize(
+    "inputtype,expected",
+    [
+        ("int8", "int64"),
+        ("integer", "int32"),
+        ("numeric", "float64"),
+        ("double precision", "float64"),
+        ("text", "string"),
+        ("uuid", "string"),
+        ("character", "string"),
+        ("tsvector", "string"),
+        ("jsonb", "string"),
+        ("varchar", "string"),
+        ("bpchar", "string"),
+        ("date", "date64"),
+        ("bool", "bool_"),
+        ("datetime", "timestamp(ms)"),
+        ("tt", "string"),
+    ],
+)
+def test_convert_to_mojap_type(inputtype: str, expected: str):
+    actual = m.convert_to_mojap_type(inputtype)
+    assert actual == expected
